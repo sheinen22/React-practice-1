6f3553f36e3c223407d17eaed870120cf8e9aeed
Stephen Heinen <stephenheinen22@yahoo.com>
2023-02-06 19:05:36 -0600

    fancy button

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 import Card from "../UI/Card";
+import Button from "../UI/Button"
 import "./AddUser.css"
 
 function AddUser(props) {
@@ -15,7 +16,7 @@ function AddUser(props) {
         <input id="username" type="text" />
         <label htmlFor="age">Age (Years)</label>
         <input id="age" type="number" />
-        <button type="submit">Add User</button>
+        <Button type="submit">Add User</Button>
       </form>
     </Card>
   );
